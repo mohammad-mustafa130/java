795795e3e72a41eb03548f079d2b665616d29cdd
Mohammad Mustafa <mustafamohd7636@gmail.com>
2026-03-03 00:30:12 +0530

    commit

---
+++ DivideKaro.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class DivideKaro {
+	public static void main(String... args) {
+		int x, y, z;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Numerator: ");
+		x = sc.nextInt();
+
+		System.out.println("Enter Denominator: ");
+		y = sc.nextInt();
+
+		z = x / y;
+		System.out.println(z);
+	}
+}
