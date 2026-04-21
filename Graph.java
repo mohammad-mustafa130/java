a2ba7aaa051f08d3d799fce6d98c97e005fa9a68
Mohammad Mustafa <mustafamohd7636@gmail.com>
2026-04-21 23:16:09 +0530

    commit

--- Graph.java
+++ Graph.java
@@ -1,15 +1,64 @@
-import java.util.*;
 class Graph {
-	private List<Integer>[] g;
-	public Graph(int v) {
-		g = (ArrayList<Integer>[]) new ArrayList[v];
+	private int Vcnt, Ecnt;
+	private boolean digraph;
 
-		for(int i = 0; i < v; i++) {
-			g[i] = new ArrayList<>();
+	private class Node {
+		int v;
+		Node next;
+		Node(int x, Node t) {
+			v = x;
+			next = t;
 		}
 	}
-	public void addEdge(int v1, int v2) {
-		g[v1].add(v2);
-		g[v2].add(v1);
+	private Node[] adj;
+	Graph(int v, boolean flag) {
+		Vcnt = v;
+		Ecnt = 0;
+		digraph = flag;
+		adj = new Node[v];
+	}
+	int v() {
+		return Vcnt;
+	}
+	int e() {
+		return Ecnt;
+	}
+	boolean directed() {
+		return digraph;
+	}
+	void insert(int v, int w) {
+		adj[v] = new Node(w, adj[v]);
+		if(!digraph) {
+			adj[w] = new Node(v, adj[w]);
+		}
+		Ecnt++;
+	}
+	AdjList getAdjList(int v) {
+		return new AdjLinkedList(v);
+	}
+	interface AdjList {
+		int beg();
+		int nxt();
+		boolean end();
+	}
+	private class AdjLinkedList implements AdjList {
+		private int v;
+		private Node t;
+		AdjLinkedList(int v) {
+			this.v = v;
+			t = null;
+		}
+		public int beg() {
+			t = adj[v];
+			return t == null ? -1 : t.v;
+		}
+		public int nxt() {
+			if(t != null) t = t.next;
+			return t == null ? -1 : t.v;
+		}
+		public boolean end() {
+			return t == null;
+		}
 	}
 }
+
--- GraphTester.java
+++ GraphTester.java
@@ -1,13 +1,17 @@
 class GraphTester {
 	public static void main(String[] args) {
-		Graph g = new Graph(5);
+		Graph g = new Graph(5, false);
 
-		g.addEdge(0,1);
-		g.addEdge(0,2);
-		g.addEdge(1,2);
-		g.addEdge(1,3);
-		g.addEdge(2,3);
-		g.addEdge(2,4);
-		g.addEdge(3,4);
+		g.insert(0,1);
+		g.insert(0,2);
+		g.insert(0,4);
+		g.insert(1,3);
+		g.insert(2,4);
+		g.insert(3,4);
+
+		GraphPath gp = new GraphPath(g, 0, 4);
+		boolean result = gp.exists();
+
+		System.out.println("Path exist: " + result);
 	}
 }
